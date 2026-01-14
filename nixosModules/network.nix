97ebcef25a081379b032b6319395d5dae0e0a493
Richard Vock <nrtn@posteo.net>
2026-01-14 10:28:09 +0100

    fail2ban

--- nixosModules/network.nix
+++ nixosModules/network.nix
@@ -20,4 +20,14 @@
       PermitRootLogin = lib.mkForce "no";
     };
   };
+
+  services.fail2ban = {
+    enable = true;
+    # ensure the sshd jail watches our custom SSH port
+    jails.sshd.settings = {
+      enabled = true;
+      filter = "sshd";
+      port = "55522";
+    };
+  };
 }
