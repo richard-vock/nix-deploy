090e50fee768df3810f7d208ccd97e6446f4830a
Richard Vock <nrtn@posteo.net>
2026-01-29 12:14:04 +0100

    fix

--- damogran/services/headscale.nix
+++ damogran/services/headscale.nix
@@ -8,7 +8,7 @@ let
   headscaleDomain = "headscale.${domain}";
   magicDnsDomain = "net.${domain}";
   listenAddress = "127.0.0.1";
-  listenPort = 8180;
+  listenPort = 8085;
 in
 {
   services.headscale = {
@@ -17,18 +17,28 @@ in
     address = listenAddress;
     port = listenPort;
 
-    dns = {
-      nameservers = [
-        "1.1.1.1"
-        "1.0.0.1"
-      ];
-      magicDns = true;
-      baseDomain = magicDnsDomain;
-    };
-
     settings = {
-      logtail = {
-        enabled = false;
+      dns = {
+        magic_dns = true;
+        base_domain = magicDnsDomain;
+        nameservers.global = [
+          "1.1.1.1"
+          "1.0.0.1"
+        ];
+      };
+      logtail.enabled = false;
+      oidc = {
+        issuer = "https://authentik.${domain}/application/o/headscale/";
+        client_id = "VzQA2VsUtmS85AzKXc6deBbiEWaRgrWs05m3dHrf";
+        client_secret_path = config.sops.secrets."headscale/oidc-client-secret".path;
+        scope = [
+          "openid"
+          "profile"
+          "email"
+        ];
+        domain_map = {
+          ".*" = "default"; # map every user to the default namespace
+        };
       };
     };
   };
@@ -37,5 +47,9 @@ in
     subdomain = "headscale";
     address = listenAddress;
     port = listenPort;
+  };
+
+  sops.secrets = {
+    "headscale/oidc-client-secret" = { };
   };
 }
