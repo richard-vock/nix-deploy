0f36ac613dbcb5463cf5b62079d75325e9d1c1c2
Richard Vock <nrtn@posteo.net>
2026-01-29 10:41:34 +0100

    cleanup

--- damogran/services/authentik.nix
+++ damogran/services/authentik.nix
@@ -10,18 +10,6 @@
     enable = true;
     outposts.ldap.enable = false;
   };
-  #
-  # services.haproxy.settings = {
-  #   frontends.ldaps = {
-  #     mode = "tcp";
-  #     bind.port = 636;
-  #     defaultBackend = "ldaps";
-  #   };
-  #   backends.ldaps = {
-  #     mode = "tcp";
-  #     servers = [ "authentik 127.0.0.1:6636" ];
-  #   };
-  # };
 
   database.authentik = { };
 
@@ -39,6 +27,5 @@
 
   sops.secrets = {
     "authentik/env" = { };
-    # authentik-ldap = { };
   };
 }
